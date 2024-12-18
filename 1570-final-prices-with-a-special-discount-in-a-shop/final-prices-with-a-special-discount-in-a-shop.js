eaa38889061bafca23ce3441e7d001e1175a7605
Diego Lopes <diego.lopes.2402@gmail.com>
2024-12-18 03:02:04 -0300

    Time: 1 ms (70.33%) | Memory: 52.1 MB (53.77%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
@@ -3,19 +3,18 @@
  * @return {number[]}
  */
 var finalPrices = function (prices) {
-    var answer = []
+    var answer = [];
 
     for (var i = 0; i < prices.length; i++) {
-        var discount = 0
+        var discount = 0;
+
         for (var j = i + 1; j < prices.length; j++) {
             if (prices[i] >= prices[j]) {
-                // New discount\
-                discount = prices[j]
+                discount = prices[j];
                 break;
             }
         }
-         answer.push(prices[i] - discount); // Apply discount and store final price
+        answer.push(prices[i] - discount)
     }
-           
     return answer
 };
