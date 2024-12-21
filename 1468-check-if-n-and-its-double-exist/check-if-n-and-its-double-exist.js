e7b5e0e36c60efdf2f80adbdd1f420875dc875db
Diego Lopes <diego.lopes.2402@gmail.com>
2024-12-21 02:24:57 -0300

    Time: 6 ms (12.99%) | Memory: 51.2 MB (26.59%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -3,12 +3,15 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
+    var map = {}
+
     for (var i = 0; i < arr.length; i++) {
-        for (var j = i + 1; j < arr.length; j++) {
-            if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
-                return true
-            }
+    
+        if (map[arr[i] * 2] !== undefined || map[arr[i] / 2] == arr[i] / 2) {
+            return true
         }
+
+        map[arr[i]] = arr[i]
     }
 
     return false
