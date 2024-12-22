1e74b1cf49b3e4f8a71248ab8cd84e027c245702
Diego Lopes <diego.lopes.2402@gmail.com>
2024-12-22 16:50:35 -0300

    Time: 2 ms (75.92%) | Memory: 60 MB (10.31%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = Infinity; // Track the minimum price seen so far
-    let maxProfit = 0; // Track the maximum profit
-    
-    for (let i = 0; i < prices.length; i++) {
-        // Update the minimum price if a lower price is found
-        minPrice = Math.min(minPrice, prices[i]);
-        
-        // Calculate profit if selling on the current day
-        let profit = prices[i] - minPrice;
-        
-        // Update maxProfit if the current profit is greater
-        maxProfit = Math.max(maxProfit, profit);
+    if (prices.length == 0) return 0
+
+    var minPrice = prices[0]
+    var maxProfit = 0
+
+    for (var i = 0; i < prices.length; i++) {
+        minPrice = Math.min(prices[i], minPrice)
+        let profit =  prices[i] - minPrice
+        maxProfit = Math.max(maxProfit, profit)
     }
-    
-    return maxProfit;
+
+    return maxProfit
 };
