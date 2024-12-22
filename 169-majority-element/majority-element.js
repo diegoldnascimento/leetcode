bb4a3000cfab3fdfcb5dbe2adb44f7d7fe87e196
Diego Lopes <diego.lopes.2402@gmail.com>
2024-12-22 03:22:54 -0300

    Time: 1803 ms (5.04%) | Memory: 51.8 MB (70.11%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -2,37 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
     var map = new Map()
     var ans = nums[0]
+    var highest = 0
 
     for (var i = 0; i < nums.length; i++) {
-        var num = nums[i]
+        var found = 0
 
-        console.log({num})
-
-        if (!map.has(num)) {
-            map.set(num, 1)
-        } else {
-            map.set(num, (map.get(num) || 0) +1)
-
-            if (map.get(num)+1 > Math.round(nums.length / 2)) {
-                return num
-            } 
-        }
-
-        var temp = null
-
-        for (const [key, val] of map.entries()) {
-            if (temp == null) {
-                temp = val
-            }
-
-            if (val >= temp) {
-                ans = key
-                temp = val
+        for (var j = i; j < nums.length; j++) {
+            if (nums[j] == nums[i]) {
+                found++
             }
         }
+
+        if (found > highest) {
+            ans = nums[i]
+            highest = found
+        }
+
+        if (highest > Math.floor(nums.length/2)) {
+            return ans
+        }
+
+        found = 0
     }
 
     return ans
