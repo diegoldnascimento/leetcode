41d401985c2e251d0724a800a6b6b54180f04b51
Diego Lopes <diego.lopes.2402@gmail.com>
2025-01-09 12:39:52 -0300

    Time: 0 ms (100.00%) | Memory: 51.5 MB (51.22%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
@@ -3,17 +3,14 @@
  * @return {number}
  */
 var findClosestNumber = function(nums) {
-    var closest = nums[0]
+    var closestNumber = nums[0];
 
-    for (var i = 1; i < nums.length; i++) {
-        const num = nums[i];
-        
-        if (Math.abs(num) < Math.abs(closest)) {
-            closest = num;
-        } else if (Math.abs(num) === Math.abs(closest) && num > closest) {
-            closest = num;
+    for (var i = 0; i < nums.length; i++) {
+         if (Math.abs(nums[i]) < Math.abs(closestNumber) || 
+            (Math.abs(nums[i]) === Math.abs(closestNumber) && nums[i] > closestNumber)) {
+            closestNumber = nums[i];
         }
     }
 
-    return closest
+    return closestNumber;
 };
