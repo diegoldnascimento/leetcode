c016b58171c780df33325483818cff0c54e1ffb0
Diego Lopes <diego.lopes.2402@gmail.com>
2025-01-09 12:41:13 -0300

    Time: 2 ms (39.82%) | Memory: 52.4 MB (6.69%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.js
@@ -5,7 +5,7 @@
 var findClosestNumber = function(nums) {
     var closestNumber = nums[0];
 
-    for (var i = 0; i < nums.length; i++) {
+    for (var i = 1; i < nums.length; i++) {
          if (Math.abs(nums[i]) < Math.abs(closestNumber) || 
             (Math.abs(nums[i]) === Math.abs(closestNumber) && nums[i] > closestNumber)) {
             closestNumber = nums[i];
