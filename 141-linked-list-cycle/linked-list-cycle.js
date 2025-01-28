5bdeadab4a7909c9065e60b7928453a6c85d1adb
Diego Lopes <diego.lopes.2402@gmail.com>
2025-01-28 01:14:29 -0300

    Time: 67 ms (44.87%) | Memory: 57.5 MB (5.17%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    var visited = new Set()
+
+    while (head) {
+        if (visited.has(head)) {
+            return true;
+        } 
+
+        visited.add(head)
+
+        if (!head.next) {
+            break;
+        }
+
+        head = head.next
+
+    }
+
+    return false;
+};
