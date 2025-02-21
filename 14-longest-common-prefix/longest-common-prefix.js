08e24cc44df7737a3728b0230dd9c494f45c4e76
Diego Lopes <diego.lopes.2402@gmail.com>
2025-02-21 01:09:02 -0300

    Time: 3 ms (38.10%) | Memory: 56 MB (5.14%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (strs.length == 1) {
+        return strs[0]
+    }
+
+    var chars = strs[0];
+
+    var longestCommonPrefix = []
+
+    for (var i = 0; i < chars.length; i++) {
+        var char = chars[i]
+        var counter = 0;
+
+        for (var j = 1; j < strs.length; j++) {
+            var char2 = strs[j][i]
+
+            if (char2 !== null && char2 === char) {
+                counter++
+
+                if (counter == strs.length - 1) {
+                    longestCommonPrefix.push(char2)
+                }
+            } else {
+                return longestCommonPrefix.join("")
+            }
+        }
+    }
+
+    return longestCommonPrefix.join("")
+
+};
