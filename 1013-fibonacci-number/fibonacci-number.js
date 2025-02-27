bfe0117508bd301abdfd61d9dc5889fde5afe753
Diego Lopes <diego.lopes.2402@gmail.com>
2025-02-27 02:19:46 -0300

    Time: 42 ms (93.44%) | Memory: 53.6 MB (8.51%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -7,6 +7,10 @@ var fib = function(n, memo = {}) {
         return n;
     }
 
+    if (memo[n] !== undefined) {
+        return memo[n]
+    }
+
     memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
 
     return memo[n]
