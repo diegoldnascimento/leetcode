fccbee73da3abf624e1b21e76c12cd854c936af8
Diego Lopes <diego.lopes.2402@gmail.com>
2025-05-05 02:09:40 -0300

    Time: 0 ms (100.00%) | Memory: 55.1 MB (48.56%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,14 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum = function(nums, target) {
+    if (nums.length === 0) {
+        return []
+    }
+
     var hashmap = new Map()
 
     for (var i = 0; i < nums.length; i++) {
-        var diff = target - nums[i]
+        var complement = target - nums[i]
 
-        if (hashmap.has(diff)) {
-            return [hashmap.get(diff), i]
+        if (hashmap.has(complement)) {
+            return [hashmap.get(complement), i]
         }
         hashmap.set(nums[i], i)
     }
