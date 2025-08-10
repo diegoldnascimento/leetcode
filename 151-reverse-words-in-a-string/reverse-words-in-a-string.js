1c685483609ad4b067e9081effc8cb10717ab155
Diego Lopes <diego.lopes.2402@gmail.com>
2025-08-10 04:04:30 -0300

    Time: 14 ms (5.77%) | Memory: 58.7 MB (5.09%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -2,6 +2,29 @@
  * @param {string} s
  * @return {string}
  */
+
+var split = function (s, delimiter) {
+    var arr = [];
+    var str = ""
+
+    for (var i = 0; i < s.length; i++) {
+        // abc ab
+
+        if (s[i] === delimiter) {
+            arr.push(str)
+            str = ""
+        } else {
+            str += s[i];
+        }
+
+        if (i === s.length - 1) {
+            arr.push(str)
+        }
+    }
+console.log(arr)
+    return arr;
+}
+
 var reverseWords = function(s) {
     // Given the string s, reverse the order of the words
     // return a string of the words in reserver order
@@ -17,7 +40,7 @@ var reverseWords = function(s) {
 
     var words = [];
     var result = [];
-    s = s.split(" ");
+    s = split(s, " ")
 
     for (var i = s.length - 1; i >= 0; i--) {
         var word = s[i];
